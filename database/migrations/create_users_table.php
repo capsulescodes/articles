34451132afc848e70757b0426612f1a50bfbdf39
mho22 <yannick@capsules.codes>
2024-06-28 17:10:32 +0200

    Improved process

--- database/migrations/create_users_table.php
+++ database/migrations/create_users_table.php
@@ -7,14 +7,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public $name = 'users';
-
     public function up() : void
     {
-        Schema::create( $this->name, function( Blueprint $table )
+        Schema::create( 'users', function( Blueprint $table )
         {
             $table->id();
-            $table->string( 'first_ame' );
+            $table->string( 'first_name' );
             $table->string( 'last_name' );
             $table->string( 'email' )->unique();
             $table->timestamp( 'email_verified_at' )->nullable();
@@ -26,6 +24,6 @@ return new class extends Migration
 
     public function down() : void
     {
-        Schema::dropIfExists( $this->name );
+        Schema::dropIfExists( 'users' );
     }
 };
