954713965979cfc8d5ab4df5a650f3b6d9b2b203
mho22 <yannick@capsules.codes>
2024-08-12 15:50:50 +0200

    Added create custom error page article

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\App;
 
 
-Route::get( '/{status?}', fn( int | null $status = null ) => App::abort( $status ?? 404 ) );
+Route::get( '/{status?}', fn( int | null $status = null ) => App::abort( $status ?? 418 ) );
